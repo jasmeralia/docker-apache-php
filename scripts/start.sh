6b096e3185a76ff67a518c4ba864c7f0caa726ef
Morgan Blackthorne <morgan@windsofstorm.net>
2017-06-14 08:45:12 -0700

    Adjust ServerName/ServerAlias settings to make apache happier

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,7 @@
 # start all the services
+if [ "x$APACHE2_HOSTNAME" != "x" ]; then
+  sed -e "s/ServerName www.example.com/ServerName $APACHE2_HOSTNAME/" /etc/apache2/sites-available/000-default.conf > /tmp/000-default.conf
+  mv /tmp/000-default.conf /etc/apache2/sites-available/000-default.conf
+fi
+
 /usr/local/bin/supervisord -n
